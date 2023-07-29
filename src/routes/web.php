efcb26276b614d4eeb4b0589077d32539754b035
megumi-matsukura <matsukuramegumi95@gmail.com>
2023-07-29 11:38:47 +0900

    QA用

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//投稿の一覧ページ
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
